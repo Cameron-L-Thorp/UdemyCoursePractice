2545b92425252c0f661835da2b75d7dd0517deaa
Cameron Thorp <cameron.thorp123@outlook.com>
2022-11-06 11:40:06 -0700

    more notes on spread/rest and destructuring

---
+++ ReactJSCourse/1JSRefresher/Destructuring.js
@@ -0,0 +1,13 @@
+//Decructuring allows for each variable in an array or object to be assigned to a variable
+const greetings = ['Good morning!', 'Good night.'];
+[greeting, goodbye] = greetings;
+//This should print out the phrases 'Good morning!' and then 'Good night.'
+console.log(greeting);
+console.log(goodbye);
+
+const Cameron = {
+    fName: 'Cameron',
+    age: 23,
+    lName: 'Thorp'
+}
+
